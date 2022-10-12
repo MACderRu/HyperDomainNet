0bf38fd78820d5c21aa1ad477387e0811736dda0
Vadim <titow2408@gmail.com>
2022-10-12 15:24:53 +0300

    Inferencer class

--- core/utils/example_utils.py
+++ core/utils/example_utils.py
@@ -7,12 +7,14 @@ from core.utils.common import get_trainable_model_state, get_stylegan_conv_dimen
 from core.parametrizations import BaseParametrization
 from core.mappers import mapper_registry
 
-
-class TextDrivenWrapper(nn.Module):
+    
+class Inferencer(nn.Module):
     def __init__(self, ckpt, device):
         super().__init__()
+        
         self.device = device
-        self.mode = ckpt['model_type']
+        self.model_type = ckpt['model_type']
+        self.da_type = ckpt['da_type']
         
         self.sg2_source = uda_models['stylegan2'](
             img_size=ckpt['sg2_params']['img_size'],
@@ -26,15 +28,16 @@ class TextDrivenWrapper(nn.Module):
         self.sg2_source.freeze_layers()
         self.sg2_source.to(self.device)
         
-        if self.mode == 'original':
-            self._model_da = uda_models['stylegan2'](
+        if self.model_type == 'original':
+            self.model_da = uda_models['stylegan2'](
                 img_size=ckpt['sg2_params']['img_size'],
                 latent_size=ckpt['sg2_params']['latent_size'],
                 map_layers=ckpt['sg2_params']['map_layers'],
                 channel_multiplier=ckpt['sg2_params']['channel_multiplier'], 
                 checkpoint_path=ckpt['sg2_params']['ckpt_path']
             )
-        elif self.mode == 'mapper':
+            self.model_da.freeze_layers()
+        elif self.model_type == 'mapper':
             self.model_da = mapper_registry[
                 ckpt['mapper_config']['mapper_type']
             ](
@@ -49,22 +52,9 @@ class TextDrivenWrapper(nn.Module):
         self.model_da.load_state_dict(ckpt['state_dict'])
         self.model_da.to(self.device)
         
-        if ckpt['da_type'] == 'im2im':
+        if self.da_type == 'im2im':
             self.style_latents = ckpt['style_latents'].to(self.device)
-    
-    @torch.no_grad()
-    def forward(self, latents, **kwargs):
-        src_imgs, _ = self.sg2_source(latents, **kwargs)
-        if self.mode == 'original':
-            trg_imgs, _ = self.model_da(latents, **kwargs)
-        elif self.mode == 'mapper':
-            trg_imgs, _ = self.sg2_source(latents, offsets=self.model_da(kwargs['mapper_input']), **kwargs)
-        else:
-            trg_imgs, _ = self.sg2_source(latents, offsets=self.model_da(), **kwargs)
-        return src_imgs, trg_imgs
-    
-
-class ImageDrivenWrapper(TextDrivenWrapper):
+        
     @torch.no_grad()
     def forward(self, latents, **kwargs):
         if not kwargs.get('input_is_latent', False):
@@ -73,17 +63,20 @@ class ImageDrivenWrapper(TextDrivenWrapper):
         
         src_imgs, _ = self.sg2_source(latents, **kwargs)
         
-        if kwargs.get('truncation', False):
-            if kwargs['truncation'] < 1:
-                latents = self._mtg_mixing_noise(latents, truncation=kwargs['truncation'])
-                kwargs.pop('truncation')
+        if not kwargs.get('truncation', False):
+            kwargs['truncation'] = 1
         
-        if self.mode == 'original':
+        if self.da_type == 'im2im':
+            latents = self._mtg_mixing_noise(latents, truncation=kwargs['truncation'])
+            kwargs.pop('truncation')
+        
+        if self.model_type == 'original':
             trg_imgs, _ = self.model_da(latents, **kwargs)
-        elif self.mode == 'mapper':
+        elif self.model_type == 'mapper':
             trg_imgs, _ = self.sg2_source(latents, offsets=self.model_da(kwargs['mapper_input']), **kwargs)
         else:
             trg_imgs, _ = self.sg2_source(latents, offsets=self.model_da(), **kwargs)
+        
         return src_imgs, trg_imgs
     
     def _mtg_mixing_noise(self, latents, truncation=1):
@@ -93,3 +86,4 @@ class ImageDrivenWrapper(TextDrivenWrapper):
         style_mixing_latents = truncation * (w_styles - gen_mean) + gen_mean
         style_mixing_latents[:, 7:, :] = self.style_latents
         return [style_mixing_latents]
+    
