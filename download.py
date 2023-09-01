1a25975c4bdea0bb386982fdc184892963a17309
Vadim <titov@airi.net>
2023-09-01 17:18:00 +0300

    fixed download script

--- download.py
+++ download.py
@@ -5,12 +5,22 @@ from pathlib import Path
 
 
 def download_curl(source: str, destination: str) -> None:
-    # urllib has troubles with dropbox
     subprocess.run(
-        ['curl', '-L', '-k', source, '-o', destination]
+        ['curl', '-L', '-k', source, '-o', destination],
+        stdout=subprocess.DEVNULL
     )
 
-
+    
+def untar(path: str, destination: str = None):
+    command = ['tar', '-xzvf', path]
+    
+    if destination is not None:
+        command += ['-C', destination]
+        if not os.path.exists(destination):
+            os.makedirs(destination)
+    subprocess.run(command, stdout=subprocess.DEVNULL)
+    
+    
 def download_gdrive(file_id: str, destination: str) -> None:
     subprocess.run(['gdown', '--id', file_id, '-O', destination])
     
@@ -20,23 +30,52 @@ def unzip(path: str, res_path: str = None):
     
     if res_path is not None:
         command += ['-d', res_path]
-    subprocess.run(command)
+    subprocess.run(command, stdout=subprocess.DEVNULL)
 
 
 def bzip2(path: str):
     subprocess.run(['bzip2', '-d', path])
-
+    
     
 def rm_file(path: str):
     subprocess.run(['rm', path])
     
 
+class Setup:    
+    def __init__(self):
+        self.pretrained_root = Path(__file__).parent / 'pretrained'
+        self.pretrained_root.mkdir(exist_ok=True)
+        
+    def _download(self, data):
+        
+        file_dest = str(self.pretrained_root / data['name'])
+        
+        if 'link' in data:
+            download_curl(data['link'], file_dest)
+        elif 'id' in data:
+            download_gdrive(data['id'], file_dest)
+        
+        if file_dest.endswith('bz2'):
+            bzip2(file_dest)
+            rm_file(file_dest)
+        elif file_dest.endswith('tar.gz'):
+            untar(file_dest, str(self.pretrained_root / data['uncompressed_dir']))
+            rm_file(file_dest)
+        elif file_dest.endswith('.zip'):
+            unzip(file_dest, str(self.pretrained_root / data['uncompressed_dir']))
+            rm_file(file_dest)
+            
+    def setup(self, values):
+        for value in values:
+            self._download(SOURCES[value])    
+
 
 SOURCES = {
-    'stylegan2': {
-        'id': '1Yr7KuD959btpmcKGAUsbAk5rPjX2MytK',
-        'name': 'stylegan2-ffhq-config-f.pt'
-    },
+    'sg2': {
+        'link': 'https://nxt.2a2i.org/index.php/s/2K3jbFD3Tg7QmHA/download/StyleGAN2.zip',
+        'name': 'StyleGAN2.zip',
+        'uncompressed_dir': ''
+    }
     'dlib': {
         'link': 'http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2',
         'name': 'shape_predictor_68_face_landmarks.dat.bz2'
@@ -54,33 +93,6 @@ SOURCES = {
         'name': 'checkpoints.zip'
     }
 }
-
-
-class Setup:    
-    def __init__(self):
-        self.destination = Path(__file__).parent / 'pretrained'
-        self.destination.mkdir(exist_ok=True)
-        
-    def _download(self, data):
-        
-        file_dest = str(self.destination / data['name'])
-        
-        if 'link' in data:
-            download_curl(data['link'], file_dest)
-        elif 'id' in data:
-            download_gdrive(data['id'], file_dest)
-        
-        if file_dest.endswith('bz2'):
-            bzip2(file_dest)
-            rm_file(file_dest)
-        
-        if file_dest.endswith('zip'):
-            unzip(file_dest, str(self.destination))
-            rm_file(file_dest)
-        
-    def setup(self, values):
-        for value in values:
-            self._download(SOURCES[value])
             
             
 @click.command()
@@ -93,3 +105,4 @@ def main(value):
 
 if __name__ == '__main__':
     main()
+    
